77be1f9a8186eccb86171191367ca901d1c76b42
Raniro Coelho <errecme@gmail.com>
2016-12-01 00:54:17 -0200

    add ajax get call and append function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
   var channel = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-  var URL = 'https://wind-bow.hyperdev.space/twitch-api/streams/';
 
   for (var i = 0; i < channel.length; i++) {
-    console.log(URL + channel[i] + '?callback=?');
+    Request(channel[i]);
   }
 });
+
+function Request(channel){
+  var URL = 'https://wind-bow.hyperdev.space/twitch-api/streams/';
+  $.ajax({
+    url: URL + channel,
+    type: 'GET',
+    dataType: 'jsonp',
+    success: function(data){
+      AppendChannelInfo(data, channel);
+    }
+  });
+}
+
+function AppendChannelInfo(data, channel) {
+  if (data.stream !== null) {
+    $(".container").append('<h3>' + data.stream.game + '<p>' + data.stream.channel.status+ '</p></h3>');
+  } else {
+    $(".container").append('<h3>' + channel + ' not streaming</h3>');
+  }
+}
